31a1814bbc77189891f3e3d0cd209646558fcca9
guilherme-silva <guilherme.cristaldo15@gmail.com>
2025-11-26 23:01:54 -0300

    fix: delete all and I'll try again

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 let buttons = document.querySelector("#buttons-container");
 let display = document.querySelector("#display");
 
-function writeOnDisplay(text) {
-    display.textContent += text;
-}
 
-buttons.addEventListener("click", (event) => {
-    let value = event.target;
-
-    if (value.dataset.type == "number" || value.dataset.type == "operator")
-        writeOnDisplay(event.target.textContent);
-})
 
